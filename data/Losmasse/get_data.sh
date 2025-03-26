dba8c692e0bb5c9f135580de52296ba14eeb02bb
Erik Bjørnsen <bjornsen.erik@gmail.com>
2025-03-26 17:36:39 +0100

    feat: added sh script for getting geological data
    feat: added sh script for getting geological data

---
+++ data/Losmasse/get_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -o superficialdeposists_shape.zip https://swift.skyhigh.iik.ntnu.no/swift/v1/c93026420d2d49c69ac937edda870119/superficial_data_public/superficialdeposists_shape.zip
+
+unzip superficialdeposists_shape.zip -d .
+
+rm superficialdeposists_shape.zip
